effb5bc532893db6b4cc7680bbcadbd78abfe65b
ChenzheXu <71326187+ChenzheXu@users.noreply.github.com>
2023-10-18 19:22:28 -0500

    Milestone 1 Step6

--- app/src/main/java/com/cs407/lab5_milestone/NotesActivity.java
+++ app/src/main/java/com/cs407/lab5_milestone/NotesActivity.java
@@ -1,14 +1,21 @@
 package com.cs407.lab5_milestone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class NotesActivity extends AppCompatActivity {
 
     private TextView welcomeTextView;
+    private ListView notesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,36 @@ public class NotesActivity extends AppCompatActivity {
 
         String welcomeMessage = "Welcome " + username + " to Notes App!";
         welcomeTextView.setText(welcomeMessage);
+        Toolbar toolbar = findViewById(R.id.notes_toolbar);
+        setSupportActionBar(toolbar);
+
+        notesListView = findViewById(R.id.notesListView);
+
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_add_note) {
+
+            return true;
+        } else if (id == R.id.action_logout) {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    }
+
+
