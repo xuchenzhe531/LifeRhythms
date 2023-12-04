c869c39f887fccb49b9c15cb43f280fb515f694e
ChenzheXu <71326187+ChenzheXu@users.noreply.github.com>
2023-12-04 11:40:44 -0600

    Revised NoteWriting UI

--- app/src/main/java/com/cs407/lab5_milestone/noteWriting.java
+++ app/src/main/java/com/cs407/lab5_milestone/noteWriting.java
@@ -8,8 +8,10 @@ import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,6 +42,12 @@ import java.util.Date;
 
 public class noteWriting extends AppCompatActivity {
     private EditText noteEditText;
+    private EditText dateEditText;
+    private EditText startTimeEditText;
+    private EditText endTimeEditText;
+    private EditText todoEditText;
+    private Spinner categorySpinner;
+
     private Button saveButton;
     private Button deleteButton;
     public int noteid = -1;
@@ -49,10 +57,21 @@ public class noteWriting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_writing);  // 设置对应的布局文件
 
-        noteEditText = findViewById(R.id.noteEditText);
+        //noteEditText = findViewById(R.id.noteEditText);
         saveButton = findViewById(R.id.saveButton);
         deleteButton = findViewById(R.id.deleteButton);
+//*********************************************************
+        dateEditText = findViewById(R.id.dateEditText);
+        startTimeEditText = findViewById(R.id.startTimeEditText);
+        endTimeEditText = findViewById(R.id.endTimeEditText);
+        todoEditText = findViewById(R.id.todoEditText);
+        categorySpinner = findViewById(R.id.categorySpinner);
 
+        String[] categories = {"Study", "Work", "Break"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter);
+//*********************************************************
         Intent intent = getIntent();
         String str = intent.getStringExtra("noteid");
         if (str != null) {
@@ -64,7 +83,23 @@ public class noteWriting extends AppCompatActivity {
         if (noteid != -1) {
             Notes note = NotesActivity.note.get(noteid);
             String noteContent = note.getContent();
-            noteEditText.setText(noteContent);
+            //noteEditText.setText(noteContent);
+            String desireDate;
+            String StartTime;
+            String endTime;
+            String todo;
+            String category;
+            String[] parts = noteContent.split("\\*");
+            desireDate = parts[0];
+            StartTime = parts[1];
+            endTime = parts[2];
+            todo = parts[3];
+            category = parts[4];
+
+            dateEditText.setText(desireDate);
+            startTimeEditText.setText(StartTime);
+            endTimeEditText.setText(endTime);
+            todoEditText.setText(todo);
         }
 
 
@@ -86,18 +121,25 @@ public class noteWriting extends AppCompatActivity {
     }
 
     public void saveMethod() {
-        String content = noteEditText.getText().toString();
+        //String content = noteEditText.getText().toString();
         String desireDate;
         String StartTime;
         String endTime;
         String todo;
         String category;
-        String[] parts = content.split("\\*");
-        desireDate = parts[0];
-        StartTime = parts[1];
-        endTime = parts[2];
-        todo = parts[3];
-        category = parts[4];
+//        String[] parts = content.split("\\*");
+//        desireDate = parts[0];
+//        StartTime = parts[1];
+//        endTime = parts[2];
+//        todo = parts[3];
+//        category = parts[4];
+        desireDate = dateEditText.getText().toString();;
+        StartTime = startTimeEditText.getText().toString();
+        endTime = endTimeEditText.getText().toString();
+        todo = todoEditText.getText().toString();
+        category = categorySpinner.getSelectedItem().toString();
+        String content = desireDate+"*"+StartTime+"*"+endTime+"*"+todo+"*"+category;
+
 
         Context context = getApplicationContext();
         SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
@@ -138,18 +180,24 @@ public class noteWriting extends AppCompatActivity {
             CalendarDBHelper CalendardbHelper = new CalendarDBHelper(sqLiteDatabaseC);
 
             String title = "CALENDAR_" + (noteid + 1);
-            String content = noteEditText.getText().toString();
+            //String content = noteEditText.getText().toString();
             String desireDate;
             String StartTime;
             String endTime;
             String todo;
             String category;
-            String[] parts = content.split("\\*");
-            desireDate = parts[0];
-            StartTime = parts[1];
-            endTime = parts[2];
-            todo = parts[3];
-            category = parts[4];
+//            String[] parts = content.split("\\*");
+//            desireDate = parts[0];
+//            StartTime = parts[1];
+//            endTime = parts[2];
+//            todo = parts[3];
+//            category = parts[4];
+            desireDate = dateEditText.getText().toString();;
+            StartTime = startTimeEditText.getText().toString();
+            endTime = endTimeEditText.getText().toString();
+            todo = todoEditText.getText().toString();
+            category = categorySpinner.getSelectedItem().toString();
+            String content = desireDate+"*"+StartTime+"*"+endTime+"*"+todo+"*"+category;
 
             dbHelper.deleteNotes(content, title);
             CalendardbHelper.deleteCalendars(desireDate,StartTime,endTime,todo,category,title);
