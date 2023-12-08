a0a1da9560f0d604dfaf25a9c54feb8c599468e5
Shaoyi Lu <slu248@wisc.edu>
2023-12-08 13:48:35 -0800

    initial commit

--- app/src/main/java/com/cs407/LifeRhythms/MainActivity.java
+++ app/src/main/java/com/cs407/LifeRhythms/MainActivity.java
@@ -5,15 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText usernameEditText;
-    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.LifeRhythms", MODE_PRIVATE);
         String username = sharedPreferences.getString("username", "");
 
-        if (!username.equals("")) { // 如果username存在，则直接打开NotesActivity
+        if (!username.isEmpty()) {
             Intent intent = new Intent(MainActivity.this, NotesActivity.class);
             startActivity(intent);
             finish();
@@ -31,26 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        usernameEditText = findViewById(R.id.username);
-        loginButton = findViewById(R.id.login);
+        EditText usernameEditText = findViewById(R.id.username);
+        Button loginButton = findViewById(R.id.login);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String enteredUsername = usernameEditText.getText().toString().trim();
+        loginButton.setOnClickListener(v -> {
+            String enteredUsername = usernameEditText.getText().toString().trim();
 
-                if (!enteredUsername.equals("")) {
-                    sharedPreferences.edit().putString("username", enteredUsername).apply();
+            if (!enteredUsername.isEmpty()) {
+                sharedPreferences.edit().putString("username", enteredUsername).apply();
 
-                    Intent intent = new Intent(MainActivity.this, WelcomePage.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    // 提示用户输入用户名
-                    Toast.makeText(MainActivity.this, "Username", Toast.LENGTH_SHORT).show();
-                }
+                Intent intent = new Intent(MainActivity.this, WelcomePage.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(MainActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show(); // 更清晰的提示信息
             }
         });
     }
 }
-
